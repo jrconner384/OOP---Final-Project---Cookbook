4c1d887d587e45ff99276224cf19286283b427d2
Jason Conner <jason.r.conner@gmail.com>
2011-12-04 20:32:07 -0600

    Added IncompatableMeasurementType and InvalidMeasurementType Exceptions

---
+++ src/exceptions/IncompatableMeasurementType.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class IncompatableMeasurementType extends Exception
+{
+	private static final long	serialVersionUID	= -8112333682868584375L;
+
+	public IncompatableMeasurementType()
+	{
+		this.printStackTrace();
+	}
+}
---
+++ src/exceptions/InvalidMeasurementType.java
@@ -0,0 +1,15 @@
+/**
+ * @author Jason Conner
+ */
+
+package exceptions;
+
+public class InvalidMeasurementType extends Exception
+{
+	private static final long	serialVersionUID	= -6568692340349338959L;
+	
+	public InvalidMeasurementType()
+	{
+		this.printStackTrace();
+	}
+}
