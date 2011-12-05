40fe5594add369c1cc17c9a305a222b145557415
Jason Conner <jason.r.conner@gmail.com>
2011-12-04 20:41:19 -0600

    Updated Ingredient with an add() method.  Measure will have to do most of the adding work but the method in Ingredient should be sufficient as it is now.

--- src/structures/Ingredient.java
+++ src/structures/Ingredient.java
@@ -68,4 +68,35 @@ public class Ingredient
 		Measure measure = new Measure(number, type);
 		this.amount = measure;
 	}
+	
+	/**
+	 * Adds the Measures of two Ingredients together.  This is done through Ingredient rather than Measure to ensure
+	 * that the Measures being added are from the same kind of Ingredient.
+	 * @param ingredient - The Ingredient to add to this Ingredient
+	 * @return A Measure object which is the combination of of both Ingredients' Measures
+	 */
+	public Measure add(Ingredient ingredient)
+	{
+		//Only two ingredients of the same type can be added.  For example, you cannot add together tomatoes and carrots
+		if(! this.getName().equals(ingredient.getName()))
+		{
+			return null;
+		}
+		
+		Measure left = this.getAmount();		//This Ingredient's Measure
+		Measure right = ingredient.getAmount();	//The passed-in Ingredient's Measure
+		
+		/* If the measure type of this and the passed-in Ingredient are the same, just add the Measure amounts together
+		 * For example, if both operands have the measurement type of 'cup', the amounts just need to get added.
+		 * If each operand has a different amount, some conversion has to be done.
+		 */
+		if(left.getType().equals(right.getType()))
+		{
+			return new Measure(left.getNumber() + right.getNumber(), left.getType());
+		}
+		else
+		{
+			return Measure.convert(left, right);
+		}
+	}
 }
